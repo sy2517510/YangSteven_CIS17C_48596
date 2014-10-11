de15309aa8f382b3a88cd047feda6303a429ed6f
Steven Yang <skewerblack@gmail.com>
2014-10-10 19:59:57 -0700

    Create CircularLL.h

---
+++ Class/LinkedList/CircularLL.h
@@ -0,0 +1,89 @@
+/*
+ * File:   LnkdLst.h
+ * Author: Steven Yang
+ * Created on October 10 2014, 8:00 PM
+ */
+
+#ifndef CIRCULARLL_H
+#define CIRCULARLL_H
+
+#include "LnkdLst.h"
+
+template<class T>
+class CircularLL : public LnkdLst<T> {
+public:
+    CircularLL(T);
+    CircularLL(const CircularLL<T>&);
+    CircularLL<T>& operator= (const CircularLL<T>&);
+    void prepend(T);
+    void append(T);
+    void extract(T);
+    void insertAfter(T, T);
+    void insertBefore(T, T);
+    T getTail() const;
+    string toString();
+    
+};
+template<class T>
+CircularLL<T>::CircularLL(T num)
+{
+    
+}
+
+template<class T>
+CircularLL<T>::CircularLL(const CircularLL<T>&)
+{
+    
+}
+
+template<class T>
+CircularLL<T>& CircularLL<T>::operator= (const CircularLL<T>&)
+{
+    
+}
+
+template<class T>
+void CircularLL<T>::prepend(T num)
+{
+    
+}
+
+template<class T>
+void CircularLL<T>::append(T num)
+{
+    
+}
+
+template<class T>
+void CircularLL<T>::extract(T num)
+{
+    
+}
+
+template<class T>
+void CircularLL<T>::insertBefore(T value, T input)
+{
+    
+}
+
+template<class T>
+void CircularLL<T>::insertAfter(T value, T input)
+{
+    
+}
+
+template<class T>
+T CircularLL<T>::getTail() const
+{
+    
+}
+
+template<class T>
+string CircularLL<T>::toString()
+{
+    string word = ' ';
+    
+    return word;
+}
+
+#endif	/* CIRCULARLL_H */
