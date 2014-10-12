b627ad8bd8700436ede3f8f5ff58f04ac02a3eef
Steven Yang <skewerblack@gmail.com>
2014-10-11 22:55:19 -0700

    Added constructor & cleaned up class
    got rid of isFull since it doesnt have a max size, added nodes and added a constructor with init

--- Class/LinkedList/Queue.h
+++ Class/LinkedList/Queue.h
@@ -1,62 +1,71 @@
 /*
- * File:   LnkdLst.h
- * Author: Steven Yang
- * Created on October 10 2014, 8:00 PM
- */
+* File:   Queue.h
+* Author: Steven Yang
+* Created on October 10 2014, 8:00 PM
+*/
 
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include "LnkdLst.h"
-
 template<class T>
 class Queue {
 public:
-    Queue();
-    void makeEmpty();
-    bool isEmpty();
-    bool isFull();
-    void enqueue(T);
-    void dequeue(T&);
-    
+	Queue();
+	Queue(T);
+	void makeEmpty();
+	bool isEmpty();
+	void enqueue(T);
+	void dequeue(T&);
+
 private:
-    int MAX_ITEMS;
+	struct Node{
+		T data;
+		Node *next;
+	};
+	Node *head;
+	Node *worker;
+	int ITEMS;
 };
 
 template<class T>
 Queue<T>::Queue()
 {
-    MAX_ITEMS = 0;
+	ITEMS = 0;
+}
+
+template<class T>
+Queue<T>::Queue(T num)
+{
+	Node * newNode = new Node;
+	newNode->data = num;
+	newNode->next = NULL;
+
+	head = newNode;
+	ITEMS = 1;
 }
 
 template<class T>
 void Queue<T>::makeEmpty()
 {
-    
+
 }
 
 template<class T>
 bool Queue<T>::isEmpty()
 {
-    return true;
-}
-
-template<class T>
-bool Queue<T>::isFull()
-{
-    return false;
+	return true;
 }
 
 template<class T>
 void Queue<T>::enqueue(T newNode)
 {
-    
+
 }
 
 template<class T>
 void Queue<T>::dequeue(T& node)
 {
-    
+
 }
 
 #endif	/* QUEUE_H */
