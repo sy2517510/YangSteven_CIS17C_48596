480d794554a0254060b40e34e454620be69915d8
Steven Yang <skewerblack@gmail.com>
2014-10-11 22:21:44 -0700

    added implementation for isEmpty
    Also got rid of isFull since there isn't a max size to the stack

--- Class/LinkedList/Stack.h
+++ Class/LinkedList/Stack.h
@@ -1,62 +1,60 @@
 /*
- * File:   LnkdLst.h
- * Author: Steven Yang
- * Created on October 10 2014, 8:00 PM
- */
+* File:   Stack.h
+* Author: Steven Yang
+* Created on October 10 2014, 8:00 PM
+*/
 
 #ifndef STACK_H
 #define STACK_H
 
-#include "LnkdLst.h"
-
 template<class T>
 class Stack {
 public:
-    Stack();
-    bool isEmpty();
-    bool isFull();
-    void push(T);
-    void pop();
-    T top();
+	Stack();
+	bool isEmpty();
+	void push(T);
+	void pop();
+	T top();
 
 private:
-    int MAX_ITEMS;
+	struct Node{
+		T data;
+		Node *next;
+	};
+	Node *head;
+	Node *worker;
+	int ITEMS;
 };
 
 template<class T>
 Stack<T>::Stack()
 {
-    MAX_ITEMS = 0;
+	ITEMS = 0;
 }
 
 template<class T>
 bool Stack<T>::isEmpty()
 {
-    return true;
-}
-
-template<class T>
-bool Stack<T>::isFull()
-{
-    return false;
+	if (ITEMS == 0) return true;
+	else{ return false; }
 }
 
 template<class T>
 void Stack<T>::push(T newNode)
 {
-    
+
 }
 
 template<class T>
 void Stack<T>::pop()
 {
-    
+
 }
 
 template<class T>
 T Stack<T>::top()
 {
-    
+
 }
 
 #endif	/* STACK_H */
