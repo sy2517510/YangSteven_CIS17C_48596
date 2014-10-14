b7ae9ba5889fa0d61ab8caaf8c85c6fe19f058d3
Steven Yang <skewerblack@gmail.com>
2014-10-13 23:34:00 -0700

    implem operator= overload
    also clarified constructors with a ;

--- Class/LinkedList/SortedLL.h
+++ Class/LinkedList/SortedLL.h
@@ -21,19 +21,54 @@ public:
 template<class T>
 SortedLL<T>::SortedLL(T num) : LnkdLst<T>(num)
 {
-
+	;
 }
 
 template<class T>
 SortedLL<T>::SortedLL(const SortedLL& other) : LnkdLst<T>(other)
 {
-
+	;
 }
 
 template<class T>
-SortedLL<T>& SortedLL<T>::operator= (const SortedLL&)
+SortedLL<T>& SortedLL<T>::operator= (const SortedLL& other)
 {
+	Node * origPtr;
+	Node * otherPtr;
+	Node * newNode;
 
+	origPtr = this->head;
+	otherPtr = other.head;
+
+	if (origPtr == NULL || otherPtr == NULL)
+	{
+		return *this;
+	}
+
+	do
+	{
+		this->worker = this->head;
+		this->head = this->head->next;
+		delete this->worker;
+	} while (this->head);
+
+	newNode = new Node;
+	newNode->data = otherPtr->data;
+	newNode->next = NULL;
+	this->head = newNode;
+	origPtr = this->head;
+
+	while (otherPtr->next != NULL)
+	{
+		otherPtr = otherPtr->next;
+		newNode = new Node;
+		newNode->data = otherPtr->data;
+		newNode->next = NULL;
+		origPtr->next = newNode;
+		origPtr = origPtr->next;
+	}
+
+	return *this;
 }
 
 template<class T>
