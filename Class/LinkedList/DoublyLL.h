65da8aef16a9f649734732788a801d0221337055
Steven Yang <skewerblack@gmail.com>
2014-10-13 23:55:08 -0700

    added toString and implem constructor
    also got rid of lnkdlst inheritance because of many issues associated with it!

--- Class/LinkedList/DoublyLL.h
+++ Class/LinkedList/DoublyLL.h
@@ -1,76 +1,104 @@
 /*
- * File:   LnkdLst.h
- * Author: Steven Yang
- * Created on October 10 2014, 8:00 PM
- */
+* File:   DoublyLL.h
+* Author: Steven Yang
+* Created on October 10 2014, 8:00 PM
+*/
 
 #ifndef DOUBLYLL_H
 #define DOUBLYLL_H
 
-#include "LnkdLst.h"
+#include <string>
+#include <iostream>
 
 template<class T>
-class DoublyLL : public LnkdLst<T> {
+class DoublyLL {
 public:
-    DoublyLL(T);
-    DoublyLL(const DoublyLL<T>&);
-    DoublyLL<T>& operator= (const DoublyLL<T>&);
-    void prepend(T);
-    void append(T);
-    void extract(T);
-    void insertAfter(T, T);
-    void insertBefore(T, T);
+	DoublyLL(T);
+	DoublyLL(const DoublyLL<T>&);
+	DoublyLL<T>& operator= (const DoublyLL<T>&);
+	void prepend(T);
+	void append(T);
+	void extract(T);
+	void insertAfter(T, T);
+	void insertBefore(T, T);
+	string toString();
 
 private:
-    Node *prev;
+	struct Node {
+		T data;
+		Node *next;
+		Node *prev;
+	}; 
+	Node * head;
+	Node * worker;
 };
 
 template<class T>
 DoublyLL<T>::DoublyLL(T num)
 {
-    
+	Node * newNode;
+	newNode = new Node;
+	newNode->data = num;
+	newNode->next = NULL;
+	newNode->prev = NULL;
+
+	head = newNode;
 }
 
 template<class T>
 DoublyLL<T>::DoublyLL(const DoublyLL<T>&)
 {
-    
+
 }
 
 template<class T>
 DoublyLL<T>& DoublyLL<T>::operator= (const DoublyLL<T>&)
 {
-    
+
 }
 
 template<class T>
 void DoublyLL<T>::prepend(T num)
 {
-    
+
 }
 
 template<class T>
 void DoublyLL<T>::append(T num)
 {
-    
+
 }
 
 template<class T>
 void DoublyLL<T>::extract(T num)
 {
-    
+
 }
 
 template<class T>
 void DoublyLL<T>::insertBefore(T value, T input)
 {
-    
+
 }
 
 template<class T>
 void DoublyLL<T>::insertAfter(T value, T input)
 {
-    
+
+}
+
+template<class T>
+string DoublyLL<T>::toString()
+{
+	string display = " ";
+	if (head){
+		worker = head;
+		do{
+			cout << "Data element in the list ->" << worker->data << endl;
+			worker = worker->next;
+		} while (worker);
+	}
+	return display;
 }
 
 #endif	/* DOUBLYLL_H */
