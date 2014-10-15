be7c74db274e58037a37af635fcaae4c7b99dc4f
Steven Yang <skewerblack@gmail.com>
2014-10-14 22:06:02 -0700

    Changed func names to better match purpose
    Added a P which means priority

--- Class/LinkedList/PriorityLL.h
+++ Class/LinkedList/PriorityLL.h
@@ -1,8 +1,8 @@
 /*
- * File:   LnkdLst.h
- * Author: Steven Yang
- * Created on October 10 2014, 8:00 PM
- */
+* File:   PriorityLL.h
+* Author: Steven Yang
+* Created on October 10 2014, 8:00 PM
+*/
 
 #ifndef PRIORITYLL_H
 #define PRIORITYLL_H
@@ -12,61 +12,69 @@
 template<class T>
 class PriorityLL : public LnkdLst<T> {
 public:
-    PriorityLL(T);
-    PriorityLL(const PriorityLL<T>&);
-    PriorityLL<T>& operator= (const PriorityLL<T>&);
-    void prepend(T);
-    void append(T);
-    void extract(T);
-    void insertAfter(T, T);
-    void insertBefore(T, T);
-    
+	PriorityLL(T);
+	PriorityLL(const PriorityLL<T>&);
+	PriorityLL<T>& operator= (const PriorityLL<T>&);
+	void prependP(T);
+	void appendP(T);
+	void extract(T);
+	void pullHighestP();
+	void insertAfterP(T, T);
+	void insertBeforeP(T, T);
+
 };
 template<class T>
 PriorityLL<T>::PriorityLL(T num)
 {
-    
+
 }
 
 template<class T>
 PriorityLL<T>::PriorityLL(const PriorityLL<T>&)
 {
-    
+
 }
 
 template<class T>
 PriorityLL<T>& PriorityLL<T>::operator= (const PriorityLL<T>&)
 {
-    
+
 }
 
 template<class T>
-void PriorityLL<T>::prepend(T num)
+void PriorityLL<T>::prependP(T num)
 {
-    
+
 }
 
 template<class T>
-void PriorityLL<T>::append(T num)
+void PriorityLL<T>::appendP(T num)
 {
-    
+
 }
 
 template<class T>
 void PriorityLL<T>::extract(T num)
 {
-    
-}
-template<class T>
-void PriorityLL<T>::insertBefore(T value, T input)
-{
-    
+
 }
 
 template<class T>
-void PriorityLL<T>::insertAfter(T value, T input)
+void PriorityLL<T>::pullHighestP()
 {
-    
+
+}
+
+template<class T>
+void PriorityLL<T>::insertBeforeP(T value, T input)
+{
+
+}
+
+template<class T>
+void PriorityLL<T>::insertAfterP(T value, T input)
+{
+
 }
 
 #endif	/* PRIORITYLL_H */
