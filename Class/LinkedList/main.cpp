17be1443be9474dbf50a1d411ba61a58b229ecf1
Steven Yang <skewerblack@gmail.com>
2014-10-15 20:42:43 -0700

    Created output tests for all the linked lists

--- Class/LinkedList/main.cpp
+++ Class/LinkedList/main.cpp
@@ -8,6 +8,9 @@
 
 int main()
 {
+	//Linked List
+	cout << "Linked List" << endl;
+	cout << "=============" << endl;
 	LnkdLst<float> list1(0);
 	LnkdLst<float> list2(list1);
 
@@ -51,7 +54,100 @@ int main()
 	cout << "================" << endl;
 	cout << "The first value is: " << list3.getHead() << endl;
 	cout << "The last value is: " << list3.getTail() << endl;
-	cout << list3.toString() << endl;
+	cout << list3.toString() << endl; 
+
+
+	//Stack
+	cout << "Stack" << endl;
+	cout << "=============" << endl;
+	Stack<float> stack(2);
+	stack.push(4);
+	stack.push(8);
+	stack.toString();
+	cout << stack.top() << endl;
+	cout << stack.size() << endl;
+	stack.pop();
+	stack.pop();
+	stack.toString(); 
+	cout << stack.size() << endl; 
+
+	//Queue
+	cout << "Queue" << endl;
+	cout << "=============" << endl;
+	Queue<float> queue(4);
+	queue.enqueue(7);
+	queue.enqueue(0);
+	queue.toString();
+	queue.makeEmpty();
+	queue.enqueue(2);
+	queue.toString(); 
+
+	//Doubly LL
+	cout << "Doubly Linked List" << endl;
+	cout << "=============" << endl;
+	DoublyLL<float> doubly(8);
+	doubly.prepend(4);
+	doubly.prepend(0);
+	doubly.append(7);
+	doubly.append(9);
+	doubly.extract(0);
+	doubly.extract(1);
+	doubly.insertBefore(9, 2);
+	doubly.insertBefore(2, 3);
+	doubly.insertAfter(4, 1);
+	doubly.insertAfter(1, 5);
+	doubly.toString(); 
+
+	DoublyLL<float> doubly2(doubly);
+	cout << "List 2" << endl;
+	doubly2.toString();
+
+	DoublyLL<float> doubly3(4);
+	cout << "List 3" << endl;
+	doubly3 = doubly2;
+	doubly3.toString(); 
+
+	//Circular LL
+	cout << "Circular Linked List" << endl;
+	cout << "=============" << endl;
+	CircularLL<float> circList(8);
+	circList.prepend(3);
+	circList.append(2);
+	circList.append(4);
+	circList.extract(2);
+	circList.insertBefore(8, 0);
+	circList.insertAfter(4, 7);
+	circList.toString();
+	cout << "This is the tail: " << circList.getTail() << endl;
+
+	CircularLL<float> circList2(circList);
+	circList2.toString();
+
+	//Sorted LL
+	cout << "Sorted Linked List" << endl;
+	cout << "=============" << endl;
+	SortedLL<float> sorted(4);
+	sorted.insert(7);
+	sorted.insert(5);
+	sorted.insert(2);
+	sorted.insert(4.5);
+	sorted.insert(9);
+	sorted.toString();
+
+	cout << "Second List" << endl;
+	SortedLL<float> sorted2(sorted);
+	sorted2.insert(3);
+	sorted2.toString();
+
+	sorted = sorted2;
+	sorted.toString(); 
+	
+	/*
+	PriorityLL<float> priority(2);
+	priority.appendP(8);
+	priority.prependP(7);
+	priority.extract(2);
+	priority.toString(); */
 
 	//To exit
 	char c;
