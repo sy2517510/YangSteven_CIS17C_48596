c04717a6f3915bcbf155afc363092d1949fc7f56
Steven Yang <skewerblack@gmail.com>
2014-11-04 23:40:40 -0800

    added sum function
    also changed savings type to float

--- Class/Recursion/recursion.h
+++ Class/Recursion/recursion.h
@@ -2,21 +2,30 @@
 
 using namespace std;
 
-int savings(float, float, int);
+float savings(float, float, int);
+int sum(int*, int);
 
 int main()
 {
+    int array[4] = {4, 8, 2, 7};
     
-    cout<< "Amount in savings is: " << sum(2500, .04, 3) << endl;
+    cout<< "Amount in savings is: " << savings(2500, .04, 3) << endl;
+    cout << "The sum is: " << sum(array, 4);
    
     return 0;
 }
 
-int savings(float pv, float i, int n)
+float savings(float pv, float i, int n)
 {
     if(n == 0) return pv;
     
     return savings(pv * (1+i), i, n-1);
 }
 
+int sum(int * a, int n)
+{
+    if(n == 1) return a[0];
+    
+    return  a[0] + sum(a + 1, n-1);
+}
 
