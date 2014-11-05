91f77659e09a0d86a0818327e4dbabba3021511a
Steven Yang <skewerblack@gmail.com>
2014-11-04 23:27:01 -0800

    changed sum to savings

--- Class/Recursion/recursion.h
+++ Class/Recursion/recursion.h
@@ -2,21 +2,21 @@
 
 using namespace std;
 
-int sum(float, float, int);
+int savings(float, float, int);
 
 int main()
 {
     
-    cout<<"The sum is: " << sum(2500, .04, 3) << endl;
+    cout<< "Amount in savings is: " << sum(2500, .04, 3) << endl;
    
     return 0;
 }
 
-int sum(float pv, float i, int n)
+int savings(float pv, float i, int n)
 {
     if(n == 0) return pv;
     
-    return sum(pv * (1+i), i, n-1);
+    return savings(pv * (1+i), i, n-1);
 }
 
 
